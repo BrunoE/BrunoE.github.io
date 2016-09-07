6e7f44cda4df7f05ac26b85e64cb1a3ab9442aa1
BrunoE <bruno.esdm04@gmail.com>
2016-09-07 16:38:16 +0100

    Update PdfGet.js

--- PdfGet.js
+++ PdfGet.js
@@ -12,7 +12,7 @@ var pdfArray = new Array();
 
 function getFile(){
 
-    var url = "https://www.dropbox.com/s/mklsnweowm89yxn/Aula1_MKT.pdf";
+    var url = "https://www.dropbox.com/u/mklsnweowm89yxn/Aula1_MKT.pdf";
     getBinaryData(url);
 }
 
