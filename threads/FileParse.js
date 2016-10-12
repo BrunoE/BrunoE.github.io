8dd16ca0849e1e787765e7c9b0f8ce8b02063c67
BrunoE <bruno.esdm04@gmail.com>
2016-10-12 17:58:27 +0100

    Update FileParse.js

--- threads/FileParse.js
+++ threads/FileParse.js
@@ -4,11 +4,14 @@
 
 self.addEventListener('message', function(e) {
   var data = e.data;
-  if(data == 'start'){
+  console.log("Entrou");
+  if(data == "start"){
+    console.log("Entrou_S");
     startPDF();
     self.postMessage('Presentation Started');
   }
   else if(data.indexOf("https://dl") != -1){
+    console.log("Entrou_HTTP");
     self.postMessage('Presentation Started');
     getBinaryData(data);
   }
