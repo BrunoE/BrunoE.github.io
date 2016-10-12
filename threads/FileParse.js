b9f0169ac8c5265d9d394d9596bd497d2db6147a
BrunoE <bruno.esdm04@gmail.com>
2016-10-12 17:19:00 +0100

    Update FileParse.js

--- threads/FileParse.js
+++ threads/FileParse.js
@@ -1,3 +1,15 @@
 /**
  * Created by Utilizador on 11-10-2016.
  */
+
+self.addEventListener('message', function(e) {
+  var data = e.data;
+  if(data == 'start'){
+    startPDF();
+    self.postMessage('Presentation Started');
+  }
+  else if(data.indexOf("https://dl") != -1){
+    self.postMessage('Presentation Started');
+    getBinaryData(data);
+  }
+}, false);
