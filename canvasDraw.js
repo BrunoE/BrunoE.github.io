ae189b313031d816d64637256b4a5782c0cc174a
BrunoE <bruno.esdm04@gmail.com>
2016-10-19 18:19:29 +0100

    Update canvasDraw.js

--- canvasDraw.js
+++ canvasDraw.js
@@ -1,3 +1,25 @@
 /**
  * Created by Utilizador on 14-10-2016.
  */
+
+var flagDraw = false;
+var flagDrawn = false;
+
+function movePoint(message){
+
+}
+
+function changeColor(message){
+  
+}
+
+function saveChanges(currentPageNumber){
+    if(flagDrawn == true){
+        flagDrawn = false;
+
+        var canvasDraw = document.getElementById('canvasDraw');
+        var drawnSlide = canvasDraw.toDataURL();
+
+        saveCanvas.splice(currentPageNumber, 1);
+        saveCanvas.splice(currentPageNumber, 0, drawnSlide);
+    }
