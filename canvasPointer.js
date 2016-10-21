791e7f5676b0512aaba902cf683c9f05fb0ade62
BrunoE <bruno.esdm04@gmail.com>
2016-10-21 10:59:35 +0100

    Add files via upload

---
+++ canvasPointer.js
@@ -0,0 +1,78 @@
+
+var xLaser, yLaser;
+var laserFlag = 0;
+var laserImage = new Image();
+var pageFlag = 0;
+var laserWidth, laserHeight;
+var pointerWidth, pointerHeight;
+
+function initLaser(){
+    if(pageFlag == 1){
+        if(laserFlag == 0){
+            laserFlag = 1;
+
+            var canvasPDF = document.getElementById('canvasPDF');
+            reloadLaser(canvasPDF.width, canvasPDF.height);
+        }
+        else if(laserFlag == 1){
+            laserFlag = 0;
+            
+            var canvasLaser = document.getElementById('canvasLaser');
+            var contextLaser = canvasLaser.getContext("2d");
+            
+            contextLaser.clearRect(0, 0, canvasLaser.width, canvasLaser.height);
+        }
+    }
+}
+
+function reloadLaser(width, height){
+    if(laserFlag == 1) {
+        
+        var canvasLaser = document.getElementById('canvasLaser');
+        var contextLaser = canvasLaser.getContext('2d');
+        canvasLaser.height = height;
+        canvasLaser.width = width;
+
+        laserWidth = width;
+        laserHeight = height;
+
+        laserImage.onload = function () {
+            var x = width / 2;
+            var y = height / 2;
+            
+            pointerWidth = this.width/2;
+            pointerHeight = this.height/2;
+            
+            xLaser = x - pointerWidth;
+            yLaser = y - pointerHeight;
+
+            contextLaser.drawImage(laserImage, x - pointerWidth, y - pointerHeight);
+        }
+        laserImage.src = 'ledOrange.png';
+    }
+}
+
+function moveLaser(message, index){
+
+    var canvasLaser = document.getElementById('canvasLaser');
+    var contextLaser = canvasLaser.getContext('2d');
+    
+    var xString = message.substring(0, index);
+    index = index + 1;
+    var yString = message.substring(index);
+    
+    var xPercentage = parseFloat(xString, 10);
+    var yPercentage = parseFloat(yString, 10);
+    
+    xPercentage = (xPercentage*1.0) / 100;
+    yPercentage = (yPercentage*1.0) / 100;
+    
+    xLaser = laserWidth * xPercentage;
+    yLaser = laserHeight * yPercentage;
+    
+    xLaser = Math.floor( xLaser );
+    yLaser = Math.floor( yLaser );
+
+    contextLaser.clearRect(0, 0, laserWidth, laserHeight);
+    contextLaser.drawImage(laserImage, xLaser, yLaser);
+}
---
+++ commandParse.js
@@ -0,0 +1,83 @@
+var whatMode = 0;
+
+function whatCommand(message){
+	if(message == "file"){
+		whatMode = 1;
+	}
+	else if(message == "laser"){
+		whatMode = 2;
+		initLaser();
+	}
+	else if(message == "draw"){
+		whatMode = 3;
+	}
+	else if(message == "exit"){
+		whatMode = 0;
+		initLaser();
+	}
+	else{
+		whatParse(message, whatMode);
+	}
+}
+
+function whatParse(message, value){
+	switch(value){
+		case 1:
+			fileParse(message);
+			break;
+		case 2:
+			laserParse(message);
+			break;
+		case 3:
+			drawParse(message);
+			break;
+		 default:
+			break;
+	}
+}
+
+function fileParse(message){
+	if(message == "start"){
+		startPDF();
+	}
+	else if(message.indexOf("https://dl") != -1){
+		getURLData(message);
+	}
+}
+
+function laserParse(message){
+	if(message == "forwards"){
+		openNextPage();
+	}
+	else if(message == "backwards"){
+		openPrevPage();
+	}
+	else if (message.indexOf("|") != -1){
+		console.log(message);
+		var index = message.indexOf("|");
+		if(index != 0){
+			moveLaser(message, index);
+		}
+	}
+}
+
+function drawParse(message){
+	if(message == "swipeLeft"){
+		openPrevPage();
+	}
+	else if(message == "swipeRight"){
+		openNextPage();
+	}
+	else if(message == "pressUp"){
+		flagDraw = false;
+	}
+	else if(message == "pressLong"){
+		flagDraw = true;
+	}
+	else if(message.indexOf("move") != -1){
+		movePoint(message);
+	}
+	else if(message.indexOf("color") != -1){
+		changeColor(message);
+	}
+}
