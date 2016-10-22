6311cb1bd874ea466de1a8cd33c69f63fa47303f
BrunoE <bruno.esdm04@gmail.com>
2016-10-22 17:54:13 +0100

    Update getPDF.js

--- getPDF.js
+++ getPDF.js
@@ -38,7 +38,7 @@ function openNextPage() {
     }
 }
 
-function openPrevPage() {
+function openPreviousPage() {
     if (currentPage > 0) {
         saveChanges(currentPage);
         currentPage = currentPage - 1;
